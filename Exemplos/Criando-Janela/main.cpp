3faf8534df9c91644e3ecee26f57e94d2869989b
Henrique Kraemer <contato@henriquekraemer.com>
2013-07-27 14:27:38 -0300

    Atualizado main.cpp

--- Exemplos/Criando-Janela/main.cpp
+++ Exemplos/Criando-Janela/main.cpp
@@ -7,12 +7,11 @@ int main(int argc, char *argv[])
     C2D2_Inicia(800,600,C2D2_JANELA, C2D2_DESENHO_PADRAO, "Criando uma Janela");
     C2D2_Botao *teclado = C2D2_PegaTeclas();
     
-    do
-     {
+    do{
           C2D2_LimpaTela();
           C2D2_Sincroniza(C2D2_FPS_PADRAO);
-     }while(!teclado[C2D2_ESC].pressionado);
+    }while(!teclado[C2D2_ESC].pressionado);
   
     C2D2_Encerra();
-return 0;
+    return 0;
 }
